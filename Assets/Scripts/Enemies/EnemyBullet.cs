b507c424ea349a216b0cbfbbe9676385e5637786
Trevor Schotsch <schotstk@dukes.jmu.edu>
2024-07-17 08:48:28 -0400

    Enemy bullet updates
    -player now has collider
    -enemy bullets now
      - have collider
      - rotate toward player
      - delete self after 10 seconds
    - i got my intellisense working

--- Assets/Scripts/Enemies/EnemyBullet.cs
+++ Assets/Scripts/Enemies/EnemyBullet.cs
@@ -6,7 +6,9 @@ public class EnemyBullet : MonoBehaviour
 {
   private GameObject player;
   private Rigidbody2D rb; 
+  private float timer;
   public float force;
+  
 
     // Start is called before the first frame update
     void Start()
@@ -19,15 +21,26 @@ public class EnemyBullet : MonoBehaviour
         //rotate toward player
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         direction.Normalize();
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle); //rotate
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90); //rotate
 
-        
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > 10) 
+        {
+          Destroy(gameObject); // bullets delete themselves after 10 seconds
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+      if (other.gameObject.CompareTag("Player"))
+      {
+        Destroy(gameObject); // bullets delete themselves after hitting player
+      }
     }
 }
